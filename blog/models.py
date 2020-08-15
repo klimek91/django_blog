b46a67852cd20f2041cb8c31ac1761c5dbc13989
Klimek <klimkowskib@gmail.com>
2020-08-15 13:39:44 +0200

    absolute urls for blog post detail

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 # Create your models here.
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
@@ -22,11 +25,10 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()              #manager domyslny
     published = PublishedManager()          #manager niestandardowy
-
-
     class Meta:
         ordering = ('-publish',)
-
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
 
