d548e6d490ee45dc06c4e11bb1605a3f658d7256
Klimek <klimkowskib@gmail.com>
2020-08-15 12:16:21 +0200

    new Post manager

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -17,9 +20,13 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()              #manager domyslny
+    published = PublishedManager()          #manager niestandardowy
+
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
